a54f1780636698fd2755badf4cac771bab51e018
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-12-09 01:12:14 +0100

    wip: horizontal count

--- day04/src/parse.rs
+++ day04/src/parse.rs
@@ -2,7 +2,7 @@
 
 use std::ascii;
 
-use derive_more::derive::{Constructor, Display, FromStr, IntoIterator};
+use derive_more::derive::{Constructor, Display, FromStr, Index, IntoIterator};
 use indoc::indoc;
 use regex::Regex;
 use tracing::{self as tea, Level, instrument};
@@ -11,14 +11,15 @@ use crate::{ErrKindDay04, Result};
 
 /// Only valid chars in the CrossWordInput.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, FromStr)]
-enum CWordChar {
+pub enum CWordChar {
         X,
         M,
         A,
         S,
 }
-#[derive(Debug, Clone, PartialEq, Eq, Constructor, IntoIterator)]
-struct CWordLine {
+#[derive(Debug, Clone, PartialEq, Eq, Constructor, IntoIterator, Index)]
+pub struct CWordLine {
+        #[into_iterator(owned, ref, ref_mut)]
         chars: Vec<CWordChar>,
 }
 impl CWordLine {
@@ -47,110 +48,31 @@ impl CWordLine {
                 Ok(CWordLine { chars: cw_chars })
         }
 
-        fn push(&mut self, cw_char: CWordChar) {
-                self.chars.push(cw_char);
-        }
-}
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Display)]
-struct SearchStateMachine {
-        state: SearchState,
-}
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Display)]
-enum SearchState {
-        Null,
-        X,
-        XM,
-        XMA,
-        S,
-        SA,
-        SAM,
-        Found,
-}
-impl SearchStateMachine {
-        /// Start a new `XMAS|SMAX` search state machine from a null value.
-        fn new() -> Self {
-                SearchStateMachine {
-                        state: SearchState::Null,
+        /// Create a new empty CWordLine. Optionally specify the length.
+        fn new_empty(length: Option<usize>) -> Self {
+                match length {
+                        Some(len) => CWordLine {
+                                chars: Vec::with_capacity(len),
+                        },
+                        None => CWordLine { chars: Vec::new() },
                 }
         }
 
-        /// Advance the SearchStaateMachine by one char. Return the new state of the machine.
-        ///
-        /// ## Note
-        /// `Found` & `Null` are equivalent for traversal logic.
-        /// It's up to the caller to operate based on the distinction.
-        fn next(&mut self, cw_char: &CWordChar) -> SearchState {
-                let new_state = match (&cw_char, self.state) {
-                        (CWordChar::X, SearchState::Null | SearchState::Found) => SearchState::X,
-                        (CWordChar::M, SearchState::X) => SearchState::XM,
-                        (CWordChar::A, SearchState::XM) => SearchState::XMA,
-                        (CWordChar::S, SearchState::XMA) => SearchState::Found,
-                        //
-                        (CWordChar::S, SearchState::Null | SearchState::Found) => SearchState::S,
-                        (CWordChar::A, SearchState::S) => SearchState::SA,
-                        (CWordChar::M, SearchState::SA) => SearchState::SAM,
-                        (CWordChar::X, SearchState::SAM) => SearchState::Found,
-                        //
-                        _ => SearchState::Null,
-                };
-
-                self.state = new_state;
-                new_state
-        }
-
-        /// Show the state that the SearchStateMachine would enter
-        ///
-        /// ## Note
-        /// `Found` & `Null` are equivalent for traversal logic.
-        /// It's up to the caller to operate based on the distinction.
-        fn preview_next(&self, cw_char: &CWordChar) -> SearchState {
-                let new_state = match (&cw_char, self.state) {
-                        (CWordChar::X, SearchState::Null | SearchState::Found) => SearchState::X,
-                        (CWordChar::M, SearchState::X) => SearchState::XM,
-                        (CWordChar::A, SearchState::XM) => SearchState::XMA,
-                        (CWordChar::S, SearchState::XMA) => SearchState::Found,
-                        //
-                        (CWordChar::S, SearchState::Null | SearchState::Found) => SearchState::S,
-                        (CWordChar::A, SearchState::S) => SearchState::SA,
-                        (CWordChar::M, SearchState::SA) => SearchState::SAM,
-                        (CWordChar::X, SearchState::SAM) => SearchState::Found,
-                        //
-                        _ => SearchState::Null,
-                };
-
-                new_state
-        }
-
-        /// Consume a SearchStateMachine and CWordChar and produce a new, advanced, SearchStateMachine.
-        ///
-        /// ## Note
-        /// `Found` & `Null` are equivalent for traversal logic.
-        /// It's up to the caller to operate based on the distinction.
-        fn evolve(mut self, cw_char: &CWordChar) -> SearchStateMachine {
-                let new_state = match (&cw_char, self.state) {
-                        (CWordChar::X, SearchState::Null | SearchState::Found) => SearchState::X,
-                        (CWordChar::M, SearchState::X) => SearchState::XM,
-                        (CWordChar::A, SearchState::XM) => SearchState::XMA,
-                        (CWordChar::S, SearchState::XMA) => SearchState::Found,
-                        //
-                        (CWordChar::S, SearchState::Null | SearchState::Found) => SearchState::S,
-                        (CWordChar::A, SearchState::S) => SearchState::SA,
-                        (CWordChar::M, SearchState::SA) => SearchState::SAM,
-                        (CWordChar::X, SearchState::SAM) => SearchState::Found,
-                        //
-                        _ => SearchState::Null,
-                };
-
-                self.state = new_state;
-                self
+        /// Push a CWordChar onto the CWordLine.
+        fn push(&mut self, cw_char: CWordChar) {
+                self.chars.push(cw_char);
         }
 }
 
 /// Parse txt input ...
 #[instrument(skip_all, ret(level = Level::TRACE))]
-pub fn parse_input(raw_input: &str) -> Result<()> {
-        // raw_input.lines().map(|line|line.chars().collect::<Vec<ascii::Char>())
-        todo!()
+pub fn parse_input(raw_input: &str) -> Result<Vec<CWordLine>> {
+        raw_input
+                .lines()
+                .inspect(|line| tea::debug!(?line))
+                .map(CWordLine::from_str)
+                .inspect(|c_line| tea::debug!(?c_line))
+                .collect()
 }
 
 /// Example use of regex crate capture for parsing.
--- day04/src/part1_lib.rs
+++ day04/src/part1_lib.rs
@@ -1,18 +1,132 @@
 //! Library code for Part 1 of Day04 of Advent of Code 2024.
 //! `bin > part1_bin.rs` will run this code along with content of `input1.txt`
 
+use derive_more::derive::Display;
 use tracing::{self as tea, Level, instrument};
 
-#[expect(unused)]
-use crate::{Result, parse::parse_input};
+use crate::{Result,
+            parse::{CWordChar, parse_input}};
 
 #[instrument(skip_all, ret(level = Level::DEBUG))]
 pub fn process_part1(input: &str) -> Result<u64> {
         tea::trace!(%input);
-        // let _parsed_input = parse_input(input)?;
+        let simplistic_input = parse_input(input)?;
+        let mut hor_count = 0;
+
+        for (i, line) in simplistic_input.iter().enumerate() {
+                let _enter = tea::debug_span!("Processing, line", ?i, ?hor_count).entered();
+                let mut search_state_machine = SearchStateMachine::new();
+                for cw_char in line.into_iter() {
+                        let new_state = search_state_machine.next(cw_char);
+                        if new_state == SearchState::FoundXMAS || new_state == SearchState::FoundSAMX {
+                                hor_count += 1;
+                        }
+                        tea::debug!(?cw_char, ?new_state, ?i);
+                }
+        }
+        println!("{}", hor_count);
         todo!();
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Display)]
+pub struct SearchStateMachine {
+        state: SearchState,
+}
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Display)]
+enum SearchState {
+        Null,
+        X,
+        XM,
+        XMA,
+        FoundXMAS,
+        S,
+        SA,
+        SAM,
+        FoundSAMX,
+}
+impl SearchStateMachine {
+        /// Start a new `XMAS|SMAX` search state machine from a null value.
+        fn new() -> Self {
+                SearchStateMachine {
+                        state: SearchState::Null,
+                }
+        }
+
+        /// Advance the SearchStaateMachine by one char. Return the new state of the machine.
+        ///
+        /// ## Note
+        /// `Found` & `Null` are equivalent for traversal logic.
+        /// It's up to the caller to operate based on the distinction.
+        fn next(&mut self, cw_char: &CWordChar) -> SearchState {
+                let new_state = match (&cw_char, self.state) {
+                        (CWordChar::X, SearchState::Null | SearchState::FoundXMAS | SearchState::FoundSAMX) => {
+                                SearchState::X
+                        }
+                        (CWordChar::M, SearchState::X | SearchState::FoundSAMX) => SearchState::XM,
+                        (CWordChar::A, SearchState::XM) => SearchState::XMA,
+                        (CWordChar::S, SearchState::XMA) => SearchState::FoundXMAS,
+                        //
+                        (CWordChar::S, SearchState::Null | SearchState::FoundSAMX | SearchState::FoundXMAS) => {
+                                SearchState::S
+                        }
+                        (CWordChar::A, SearchState::S | SearchState::FoundXMAS) => SearchState::SA,
+                        (CWordChar::M, SearchState::SA) => SearchState::SAM,
+                        (CWordChar::X, SearchState::SAM) => SearchState::FoundSAMX,
+                        //
+                        _ => SearchState::Null,
+                };
+
+                self.state = new_state;
+                new_state
+        }
+
+        /// Show the state that the SearchStateMachine would enter
+        ///
+        /// ## Note
+        /// `Found` & `Null` are equivalent for traversal logic.
+        /// It's up to the caller to operate based on the distinction.
+        fn preview_next(&self, cw_char: &CWordChar) -> SearchState {
+                let new_state = match (&cw_char, self.state) {
+                        (CWordChar::X, SearchState::Null | SearchState::FoundXMAS) => SearchState::X,
+                        (CWordChar::M, SearchState::X | SearchState::FoundSAMX) => SearchState::XM,
+                        (CWordChar::A, SearchState::XM) => SearchState::XMA,
+                        (CWordChar::S, SearchState::XMA) => SearchState::FoundXMAS,
+                        //
+                        (CWordChar::S, SearchState::Null | SearchState::FoundSAMX) => SearchState::S,
+                        (CWordChar::A, SearchState::S | SearchState::FoundXMAS) => SearchState::SA,
+                        (CWordChar::M, SearchState::SA) => SearchState::SAM,
+                        (CWordChar::X, SearchState::SAM) => SearchState::FoundSAMX,
+                        //
+                        _ => SearchState::Null,
+                };
+
+                new_state
+        }
+
+        /// Consume a SearchStateMachine and CWordChar and produce a new, advanced, SearchStateMachine.
+        ///
+        /// ## Note
+        /// `Found` & `Null` are equivalent for traversal logic.
+        /// It's up to the caller to operate based on the distinction.
+        fn evolve(mut self, cw_char: &CWordChar) -> SearchStateMachine {
+                let new_state = match (&cw_char, self.state) {
+                        (CWordChar::X, SearchState::Null | SearchState::FoundXMAS) => SearchState::X,
+                        (CWordChar::M, SearchState::X | SearchState::FoundSAMX) => SearchState::XM,
+                        (CWordChar::A, SearchState::XM) => SearchState::XMA,
+                        (CWordChar::S, SearchState::XMA) => SearchState::FoundXMAS,
+                        //
+                        (CWordChar::S, SearchState::Null | SearchState::FoundSAMX) => SearchState::S,
+                        (CWordChar::A, SearchState::S | SearchState::FoundXMAS) => SearchState::SA,
+                        (CWordChar::M, SearchState::SA) => SearchState::SAM,
+                        (CWordChar::X, SearchState::SAM) => SearchState::FoundSAMX,
+                        //
+                        _ => SearchState::Null,
+                };
+
+                self.state = new_state;
+                self
+        }
+}
 #[cfg(test)]
 mod tests {
         use indoc::indoc;
@@ -33,6 +147,15 @@ mod tests {
                 assert_eq!(process_part1(input)?, expected);
                 Ok(())
         }
+
+        // let input = indoc!("
+        //         SAMXMASS
+        //         XMASAMXX
+        //         XMASSAMX
+        //         XSSXXXXX
+        //         XXXXXMAS
+        //         ")
+        // let expected_horizontal = 7;
 
         // /// Test's expected value to be populated after solution verification.
         // /// NOTE: `#[ignore]` is set for this test by default.
