f9ed371416042001242a50222e35ce61a4b1323a
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-12-11 12:59:45 -0500

    perf(xp): quick compare; need compiler non-opts

---
+++ day04/src/bin/4_1.rs
@@ -0,0 +1,9 @@
+//! Day04 Part1 Bench
+
+use day04::{FINAL_INPUT, Result, process_part1};
+
+fn main() -> Result<()> {
+        let val = process_part1(FINAL_INPUT)?;
+        println!("Day04 Part2: {}", val);
+        Ok(())
+}
---
+++ day04/src/bin/4_2.rs
@@ -0,0 +1,9 @@
+//! Day04 Part2 Bench
+
+use day04::{FINAL_INPUT, Result, process_part2};
+
+fn main() -> Result<()> {
+        let val = process_part2(FINAL_INPUT)?;
+        println!("Day04 Part2: {}", val);
+        Ok(())
+}
